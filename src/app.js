273cf24fdfebd33f0868437ce358e85e56f5feec
Debraj Rakshit <debrajrakshit@Debrajs-MacBook-Air.local>
2019-10-17 18:23:59 +0530

    remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -1,36 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import getVisibleExpenses from './selectors/expenses';
-import { addExpense, removeExpense, editExpense } from './actions/expenses';
-import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from './actions/filters';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-
-// getVisibleExpenses -> print visible ones to screen
-store.subscribe(() => {
-    const state = store.getState();
-    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-    console.log(visibleExpenses);
-    console.log('visibilityExpenses');
-});
-
-// addExpense -> Water bill
-const waterBill = store.dispatch(addExpense({description: 'Water bill', amount: 100, createdAt: 1000 }));
-const gasBill = store.dispatch(addExpense({ description: 'Gas bill', amount: 120, createdAt: 1200 }));
-const houseRent = store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
-// store.dispatch(setTextFilter('water'));
-
-// setTimeout(() => {
-//     store.dispatch(setTextFilter('bill'));
-// }, 3000);
-
-// console.log(store.getState());
 
 const jsx = (
     <Provider store={store}>
